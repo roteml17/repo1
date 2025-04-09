994a16c7f398b1825bd14bbaad02d1b84d1cb734
roteml17 <rotemlahav2@gmail.com>
2025-04-09 13:45:41 +0300

    changed rotemlahav.java print

--- rotemlahav.java
+++ rotemlahav.java
@@ -1,6 +1,6 @@
-public class MyName{
+public class rotemlahav{
 	public static void main(String[] args){
 		for (int i=1; i<=10; i++)
-			System.out.println("rotem lahav");
+			System.out.println("changed the print to only rotem");
 	}
 }
